3c2cf10452f7a10781d1c508d63a96c17535f039
redpix <dzavashviligeorge@gmail.com>
2017-01-28 17:45:01 +0400

    dasds

--- bot/db.py
+++ bot/db.py
@@ -5,7 +5,7 @@ config = {
   "authDomain": "spektra-80eda.firebaseapp.com",
   "databaseURL": "https://spektra-80eda.firebaseio.com",
   "storageBucket": "spektra-80eda.appspot.com",
-  "serviceAccount": "admin.json"
+  "serviceAccount": "/admin.json"
 }
 
 firebase = pyrebase.initialize_app(config)
